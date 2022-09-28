b76a326c54e5063d0becef1832c333d6d99ff6c6
Mostafa1682002 <ma9856603@gmail.com>
2022-09-28 18:36:56 +0200

    Commit three

--- js/script.js
+++ js/script.js
@@ -76,6 +76,50 @@ closeFav.addEventListener('click',()=>{
 })
 
 
+
+
+// Swiper Trusted Partners
+var swiper = new Swiper(".content-trusted ", {
+    loop:true,
+    spaceBetween: 20,
+    autoplay:{
+        delay:7500,
+        disableOnInteraction:false,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        1020: {
+            slidesPerView: 5,
+        },
+    },
+});
+
+// Swiper Clients
+var swiper2 = new Swiper(".content-clients ", {
+    loop:true,
+    spaceBetween: 20,
+    autoplay:{
+        delay:5500,
+        disableOnInteraction:false,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1020: {
+            slidesPerView: 3,
+        },
+    },
+});
+
 // 'use strict';
 // /**
 //  * navbar toggle
